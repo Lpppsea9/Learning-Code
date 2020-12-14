b52e6edb6b13cee39c1958d00768a8ebfe7c7eaa
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-14 17:29:02 +0800

    12/14

---
+++ 第13章-模块化/15/src/Style.js
@@ -0,0 +1,9 @@
+export default class Style{
+    constructor() {}
+    init() {
+        document.body.style.backgroundColor = 'powderblue'
+    }
+}
+/* 
+    这个模块的功能是改变body的背景颜色
+*/
---
+++ 第13章-模块化/15/src/index.js
@@ -0,0 +1,7 @@
+import Style from './Style';
+new Style().init();
+
+/* 
+    webpack编译时的入口文件
+    引入一个模块
+*/
