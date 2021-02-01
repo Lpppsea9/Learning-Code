c832dcef6939e98cd4794bbd37d5c4bbb33ab930
Lpppsea9 <lpppsea9@foxmail.com>
2021-02-01 18:46:12 +0800

    2/1

---
+++ my-app2/src/counter.js
@@ -0,0 +1,14 @@
+import React,{ Component, Fragment } from 'react'
+
+class Counter extends Component {
+    render() {
+        return (
+            <Fragment>
+                <button>增加</button>
+                <div>1</div>
+            </Fragment>
+        )
+    }
+}
+
+export default Counter
