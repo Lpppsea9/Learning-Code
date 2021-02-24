bbeafcf178cf0d0c12d30ac79f0aafa1c573518d
Lpppsea9 <lpppsea9@foxmail.com>
2021-02-24 15:57:34 +0800

    2/24

--- hupu-JS/JS/实操广度.js
+++ hupu-JS/JS/实操广度.js
@@ -1,3 +1,39 @@
+/*检索并计算属于同一教室中每个学生的平均分数，例子中教室 ID 为 75。每个学生可以在一年内参加一门或多门课程。以下 API 可用于检索所需数据。*/
+// GET LIST OF ALL THE STUDENTS
+GET / api / students
+Response:
+[{
+    "id": 1,
+    "name": "John",
+    "classroomId": 75
+}]
+
+// GET COURSES FOR GIVEN A STUDENT
+GET / api / courses ? filter = studentId eq 1
+Response:
+[{
+    "id": "history",
+    "studentId": 1
+}, {
+    "id": "algebra",
+    "studentId": 1
+},]
+
+// GET EVALUATION FOR EACH COURSE
+GET / api / evaluation / history ? filter = studentId eq 1
+Response: {
+    "id": 200,
+    "score": 50,
+    "totalScore": 100
+}
+
+/*编写一个接受教室 ID 的函数，并根据该函数计算该教室中每个学生的平均值。 该函数的最终输出应该是带有平均分数的学生列表：*/
+
+[
+    { "id": 1, "name": "John", "average": 70.5 },
+    { "id": 3, "name": "Lois", "average": 67 },
+]
+
 import {
     getStudents,
     getAllCourses,
