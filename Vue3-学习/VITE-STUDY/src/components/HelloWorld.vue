7705687bc0b91da85b9353941c28d21b6535fe7d
Lpppsea9 <lpppsea9@foxmail.com>
2021-04-14 17:11:42 +0800

    04/14

---
+++ Vue3-学习/VITE-STUDY/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module</p>
+  <p>Hello, vite!!!</p>
+  <p>{{ counter }}</p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
