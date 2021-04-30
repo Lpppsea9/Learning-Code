329b46d54e4608d672634072ba4a7890fb018e2a
Lpppsea9 <lpppsea9@foxmail.com>
2021-04-30 11:17:46 +0800

    04/30

--- Vue3-学习/Vue3-MOOC/jingdong-cli/src/router/index.js
+++ Vue3-学习/Vue3-MOOC/jingdong-cli/src/router/index.js
@@ -1,20 +1,41 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
+import Home from '../views/home/Home'
+import Login from '../views/login/Login'
+import Register from '../views/register/Register'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    // 需求: 如果已经登录了(isLogin=true),再去访问Login页面,跳转到首页而不是登录页
+    // beforeEnter 指的是只有你访问 Login 这个页面之前才会执行这个函数
+    beforeEnter(to, from, next) {
+      // to 指即将跳转到的页面,from 指的是 上一个页面是哪里
+      const { isLogin } = localStorage
+      isLogin ? next({ name: 'Home' }) : next()
+      // if (isLogin) {
+      //   next({name: 'Home'})
+      // } else {
+      //   // 正常显示当前的页面
+      //   next()
+      // }
+      // console.log(to, from)
+    }
+  }, {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    beforeEnter(to, from, next) {
+      const { isLogin } = localStorage
+      isLogin ? next({ name: 'Home' }) : next()
+      // console.log(to, from)
+    }
+  }
 ]
 
 const router = createRouter({
@@ -22,4 +43,27 @@ const router = createRouter({
   routes
 })
 
+// router.beforeEach 指的是不管什么路由做一个切换的时候,它都会执行
+router.beforeEach((to, from, next) => {
+  /* 1. 普通的写法,实现功能 */
+  // const isLogin = localStorage.isLogin
+  // if (isLogin || to.name === 'Login') { // 如果已经在Login页面了, isLogin=false, 会陷入死循环
+  //   next() // 如果满足条件,则可以向下执行,访问相关页面
+  // } else {
+  //   next({ name: 'Login' }) // 如果没有登录, 就跳到Login这个页面
+  // }
+
+  /* 2. 使用解构写法,三元运算符,实现功能 */
+  const { isLogin } = localStorage;
+  const { name } = to;
+  const isLoginOrRegister = (name === "Login" || name === "Register");
+  (isLogin || isLoginOrRegister) ? next(): next({ name: 'Login' })
+})
+/*
+  总结:
+    next() 可以理解成往里面传一些网页 next({name: 'Login'}),中间件执行
+  两个知识点:
+    1. 每一个路由项目里面都可以加一个 beforeEnter 这样的函数, 这个函数在你进入到这个路由之前会被执行
+    2. router 上面整体可以加一个 beforeEach 这样的一个函数, 这个函数会在路由每次做切换的时候执行,用这两个函数我们就可以做一些权限上的校验
+*/
 export default router
---
+++ Vue3-学习/Vue3-MOOC/jingdong-cli/src/views/home/StaticPart.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="position">
+      <span class="iconfont position__icon">&#xe70b;</span>
+      上海理工大学国防科技园2号楼10层上海理工大学国防科技园2号楼10层
+      <span class="iconfont position__notice">&#xecaa;</span>
+    </div>
+
+  <div class="search">
+    <div class="iconfont">&#xe613;</div>
+    <input value="山姆会员商店优惠商品" class="search__text" />
+  </div>
+
+  <div class="banner">
+    <img
+      class="banner__img"
+      src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+    />
+  </div>
+
+  <div class="icons">
+    <div class="icons__item" v-for="item in iconsList" :key="item.id">
+      <img
+        class="icons__item__img"
+        :src="`http://www.dell-lee.com/imgs/vue3/${item.imgName}.png`"
+      />
+      <p class="icons__item__desc">{{item.desc}}</p>
+    </div>
+  </div>
+
+  <div class="gap"></div>
+</template>
+
+<script>
+export default {
+  name: 'StaticPart',
+  setup () {
+    const iconsList = [
+      { imgName: '超市', desc: '超市便利' },
+      { imgName: '菜市场', desc: '菜市场' },
+      { imgName: '水果店', desc: '水果店' },
+      { imgName: '鲜花', desc: '鲜花种植' },
+      { imgName: '医药健康', desc: '医药健康' },
+      { imgName: '家居', desc: '烘培蛋糕' },
+      { imgName: '蛋糕', desc: '超市便利' },
+      { imgName: '签到', desc: '签到' },
+      { imgName: '大牌免运', desc: '大牌免运' },
+      { imgName: '红包', desc: '红包套餐' }
+    ]
+    return { iconsList }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+@import '../../style/mixin.scss';
+.position {
+  color:$content-fontcolor;
+  overflow: hidden;
+  position: relative;
+  // margin-right: .24rem; //不用margin,否则🔔图标也会向右偏移
+  padding: .16rem .24rem .16rem 0 ;
+  line-height: .22rem;
+  font-size: .16rem;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: .01rem;
+    font-size: .2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+}
+.search {
+  display: flex;
+  margin-bottom: .12rem;
+  height: .32rem;
+  line-height: .32rem;
+  background: #f5f5f5;
+  color: #B7B7B7;
+  border-radius: .16rem;
+  .iconfont {
+    width: .16rem;
+    position: relative;
+    display: inline-block;
+    font-size: .16rem;
+    padding: 0 .12rem 0 .16rem;
+  }
+  &__text {
+    width: 2.9rem;
+    display: inline-block;
+    font-size: .14rem;
+    border: none;
+  }
+  input {
+    flex: 1;
+    // background: green ;
+    background: #f5f5f5;
+    border-radius: 0 .16rem .16rem 0;
+    width: 100%;
+    border: 0;
+    outline: none;
+    letter-spacing: normal;
+    word-spacing: normal;
+    text-transform: none;
+    text-indent: 0;
+    text-shadow: none;
+    cursor: text;
+    margin: 0rem;
+    padding: 10px 0 10px 0;
+    overflow: hidden;
+  }
+}
+.banner {
+  // 防止轮播区域抖动,影响到下面的元素
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
+}
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: .06rem 0 .16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  margin: 0 -.18rem;
+  height: .1rem;
+  background: $content-bgColor;
+}
+</style>
